7103020cdff9a47fe890538f291879e748d16d1a
Kevin Castillo <kevincastillo318@gmail.com>
2020-01-24 15:27:28 +0000

    people is fetched

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,14 +3,38 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
+		<nav className="navbar navbar-light bg-light">
+			<a className="navbar-brand" href="#">
+				<img
+					src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlqRZMjCFdLQauXHu1QRX9TgC2tXBsFgA79_GnoGfiVaO__4-n&s"
+					width="100px"
+					height="50px"
+					alt="star wars"
+				/>
+			</a>
+			<div className="dropdown show">
+				<a
+					className="btn btn-dark dropdown-toggle"
+					href="#"
+					role="button"
+					id="dropdownMenuLink"
+					data-toggle="dropdown"
+					aria-haspopup="true"
+					aria-expanded="false">
+					Dropdown link
+				</a>
+
+				<div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+					<a className="dropdown-item pl-2 pr-1" href="#">
+						Jedi
+					</a>
+					<a className="dropdown-item pl-2 pr-1" href="#">
+						Sith
+					</a>
+					<a className="dropdown-item pl-2 pr-1" href="#">
+						Something else here
+					</a>
+				</div>
 			</div>
 		</nav>
 	);
--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,18 +1,8 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			demo: [],
+			people: []
 		},
 		actions: {
 			// Use getActions to call a function within a fuction
@@ -20,26 +10,15 @@ const getState = ({ getStore, getActions, setStore }) => {
 				getActions().changeColor(0, "green");
 			},
 			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
 				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
+				fetch("https://swapi.co/api/people/")
+					.then(response => response.json())
+					.then(data => {
+						setStore({ people: data.results });
+					});
+				console.log(store.demo);
 			}
 		}
 	};
 };
-
 export default getState;
